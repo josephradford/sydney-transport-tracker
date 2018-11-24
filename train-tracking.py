9272b5f112322fdffb605773872d7b0b949a9230
Joseph Radford <joeradford@gmail.com>
2018-11-24 20:23:01 +1100

    Fetch realtime dataset instead, only print entities with stop time vals

--- train-tracking.py
+++ train-tracking.py
@@ -3,12 +3,16 @@ import urllib
 
 # https://developers.google.com/transit/gtfs-realtime/examples/python-sample
 
+# The goal of this script is to record delays against stops and trips ids and route ids
+
 feed = gtfs_realtime_pb2.FeedMessage()
-req = urllib.request.Request('https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/sydneytrains')
+req = urllib.request.Request('https://api.transport.nsw.gov.au/v1/gtfs/realtime/sydneytrains')
 f = open("credentials.txt", 'r')
 apikey = f.read()
 req.add_header('Authorization', 'apikey ' + apikey)
 response = urllib.request.urlopen(req)
 feed.ParseFromString(response.read())
 for entity in feed.entity:
-    print(entity)
+    if entity.HasField('trip_update'):
+        if len(entity.trip_update.stop_time_update) > 0:
+            print(entity)
