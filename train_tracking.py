2b90eff91ea6ec0dc90669d492c72b9f6909e3ad
Joseph Radford <joeradford@gmail.com>
2018-11-29 21:13:18 +1100

    Notes on future framework of main train tracking script

--- train_tracking.py
+++ train_tracking.py
@@ -5,8 +5,15 @@ s = sched.scheduler(time.time, time.sleep)
 data_dir = "data/" + time.strftime("%Y%m%d", time.localtime())
 
 def run_downloads():
+    # loop from 4am to 2am
     s.enter(60, 1, run_downloads)
     download_delayed_trips(data_dir)
+
+    # parse all results after 2am
+
+    # analyse results after results are parsed
+
+    # exit application (or sleep until 4am)
 
 if __name__== "__main__":
     if not os.path.exists(data_dir):
