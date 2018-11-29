d71f7198878138da90e476b28cb57c28e365e25a
Joseph Radford <joeradford@gmail.com>
2018-11-29 20:51:20 +1100

    Override equality for StopTimeUpdate so it can be compared with itself

--- trip_objects.py
+++ trip_objects.py
@@ -8,6 +8,23 @@ class StopTimeUpdate:
         self.stop_id = stop_id
         self.schedule_relationship = schedule_relationship
 
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, StopTimeUpdate):
+            return ((self.arrival_delay == other.arrival_delay) and
+                    (self.departure_delay == other.departure_delay) and
+                    (self.stop_id == other.stop_id) and
+                    (self.schedule_relationship == other.schedule_relationship))
+
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Overrides the default implementation (unnecessary in Python 3)"""
+        x = self.__eq__(other)
+        if x is not NotImplemented:
+            return not x
+        return NotImplemented
+
 class TripUpdate:
     def __init__(self, trip_id, route_id, schedule_relationship, timestamp):
         self.stop_time_updates = []
