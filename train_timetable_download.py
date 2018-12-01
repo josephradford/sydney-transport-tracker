8712ba9450cb59124923cd4d5150db5b00fcccd7
Joseph Radford <joeradford@gmail.com>
2018-12-02 10:54:01 +1100

    Download request in chunks, separate unzipping into own method

--- train_timetable_download.py
+++ train_timetable_download.py
@@ -17,19 +17,35 @@ def download_timetable(data_dir):
     url = 'https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs'
     headers = {'Authorization' : 'apikey ' + apikey}
 
-    r = requests.get(url, headers=headers)
-    print(r)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    print(z)
+    req = urllib.request.Request(url)
+    req.add_header('Authorization', 'apikey ' + apikey)
+    response = urllib.request.urlopen(req)
+
+    file_name = data_dir + "/gtfs_static.zip"
+    with open(file_name, "wb") as f:
+        while (not response.fp.closed and response.fp.readable()):
+            content = response.fp.read(4096)
+
+            if len(content) > 0:
+                f.write(content)
+            else:
+                print("no content")
+                response.fp.close()
+            
+
+    print(response)
+
+def unzip_timetable(data_dir):
+    file_name = data_dir + "/gtfs_static.zip"
+    with open(file_name, "rb") as f:
+        z = zipfile.ZipFile(io.BytesIO(f.readAll()))
+    
     z.extractall()
 
-    print(r)
-
-    #pickle.dump(trips, open(data_dir + "/" + time.strftime("%H%M%S", trips.time_started) + ".pickle", "wb" ))
-    #print(timeStr() + ": Found " + str(len(trips.trip_updates)) + " trips")
 
 if __name__== "__main__":
     data_dir = "data/" + time.strftime("%Y%m%d", time.localtime())
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
     download_timetable(data_dir)
+    unzip_timetable(data_dir)
