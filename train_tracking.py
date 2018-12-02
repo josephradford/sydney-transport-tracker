004266e05db40f340601fff21e495ed075ada776
Joseph Radford <joeradford@gmail.com>
2018-12-02 22:26:34 +1100

    Set up state machine for 24 hour download and analysis process

--- train_tracking.py
+++ train_tracking.py
@@ -1,52 +1,58 @@
 from train_delay_download import *
-from collate_train_delays import *
+from collate_train_delays import collate_train_delays_run
 from analyse_by_route import *
+from static_train_timetable import static_train_timetable_run
 import sched, time, os
     
 s = sched.scheduler(time.time, time.sleep)
 data_dir = "data/"
 date_str =  time.strftime("%Y%m%d", time.localtime())
-analysed_today = False
+state = 'prepare'
 
 def run_downloads():
-    global analysed_today
+    global state
     global date_str
-    # loop from 4am to 2am
-    s.enter(60, 1, run_downloads)
-    
+
     hour = time.localtime().tm_hour
 
-    if hour > 4:
-        download_delayed_trips(data_dir + date_str)
-        analysed_today = False
-    else:
-        if not analysed_today:
-            # put all data in one file
-            collate_train_delays(data_dir)
-
-            # analyse routes
-            analyse_by_route(data_dir, date_str)
-
+    if state == 'prepare':
+        if hour > 3:
             # create the new data directory
             date_str = time.strftime("%Y%m%d", time.localtime())
             if not os.path.exists(data_dir + date_str):
                 os.makedirs(data_dir + date_str)
 
-            # stop the analysis from happening again
-            analysed_today = True
+            # get today's timetable        
+            static_train_timetable_run(data_dir, date_str)
+            s.enter(1, 1, run_downloads)
+            state = 'download_delays'
 
-    
+        else:
+            s.enter(60 * 5, 1, run_downloads)
 
-    # parse all results after 2am
+    elif state == 'download_delays':
+        s.enter(60, 1, run_downloads)
+        download_delayed_trips(data_dir + date_str)
+        if hour > 1 and hour < 2:
+            # we've rolled over to 1 am, time to collate everything
+            state = 'collate_analyse' 
 
-    # analyse results after results are parsed
+    elif state == 'collate_analyse':
+        # put all data in one file
+        collate_train_delays_run(data_dir, date_str)
+        # analyse routes
+        analyse_by_route(data_dir, date_str)
+        
+        # now we wait for tomorrow to start
+        state = 'prepare'
+        s.enter(60 * 5, 1, run_downloads)
 
-    # exit application (or sleep until 4am)
+    else:
+        s.enter(60 * 5, 1, run_downloads)
+        state = 'prepare'
 
 
 if __name__== "__main__":
-    date_str = time.strftime("%Y%m%d", time.localtime())
-    if not os.path.exists(data_dir + date_str):
-        os.makedirs(data_dir + date_str)
+    state = 'prepare'
     s.enter(1, 1, run_downloads)
     s.run()
