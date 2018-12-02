c3db1a674dfcb2db78fa54aba88d5ffa06b61151
Joseph Radford <joeradford@gmail.com>
2018-12-02 17:23:27 +1100

    Split data directory and date string in main script

--- train_tracking.py
+++ train_tracking.py
@@ -4,19 +4,20 @@ from analyse_by_route import *
 import sched, time, os
     
 s = sched.scheduler(time.time, time.sleep)
-data_dir = "data/" + time.strftime("%Y%m%d", time.localtime())
+data_dir = "data/"
+date_str =  time.strftime("%Y%m%d", time.localtime())
 analysed_today = False
 
 def run_downloads():
-    global data_dir
     global analysed_today
+    global date_str
     # loop from 4am to 2am
     s.enter(60, 1, run_downloads)
     
     hour = time.localtime().tm_hour
 
     if hour > 4:
-        download_delayed_trips(data_dir)
+        download_delayed_trips(data_dir + date_str)
         analysed_today = False
     else:
         if not analysed_today:
@@ -24,12 +25,12 @@ def run_downloads():
             collate_train_delays(data_dir)
 
             # analyse routes
-            analyse_by_route(data_dir)
+            analyse_by_route(data_dir, date_str)
 
             # create the new data directory
-            data_dir = "data/" + time.strftime("%Y%m%d", time.localtime())
-            if not os.path.exists(data_dir):
-                os.makedirs(data_dir)
+            date_str = time.strftime("%Y%m%d", time.localtime())
+            if not os.path.exists(data_dir + date_str):
+                os.makedirs(data_dir + date_str)
 
             # stop the analysis from happening again
             analysed_today = True
@@ -44,7 +45,8 @@ def run_downloads():
 
 
 if __name__== "__main__":
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+    date_str = time.strftime("%Y%m%d", time.localtime())
+    if not os.path.exists(data_dir + date_str):
+        os.makedirs(data_dir + date_str)
     s.enter(1, 1, run_downloads)
     s.run()
