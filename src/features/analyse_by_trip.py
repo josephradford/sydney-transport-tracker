5a34605c6f7d020269731da341a840e1d3a1fdea
Joseph Radford <joeradford@gmail.com>
2018-12-10 21:38:02 +1100

    Import trip summaries and filter by parsed times

--- src/features/analyse_by_trip.py
+++ src/features/analyse_by_trip.py
@@ -2,37 +2,53 @@ import pickle
 import time
 from trip_objects import *
 import os
-from progress.bar import Bar
 import csv
+from datetime import time, datetime
+import logging
+import pandas as pd
+import sys
 
-def analyse_by_trip(data_dir):
-    print("Analysing trips in " + data_dir)
-    try:
-        trips = pickle.load(open(data_dir + "/collated_delays.pickle", "rb" ))
-    except:
+
+def analyse_by_trip(_interim_data_dir, start_time, end_time):
+
+    if type(start_time) is not time:
         return
-    
-    print("Parsing timetable in " + data_dir)
+    if type(end_time) is not time:
+        return
 
-    with open(data_dir + '/stop_times.txt', mode='r', encoding='utf-8-sig') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-            else:
-                for trip in trips:
-                    if row['trip_id'] == trip.trip_id:
-                        print("Trip id match")
-                        break
-            line_count += 1
-        print(f'Processed {line_count} lines.')
+    start_time = datetime(datetime.today().year,
+                          datetime.today().month,
+                          datetime.today().day,
+                          start_time.hour,
+                          start_time.minute,
+                          start_time.second)
 
-    print("Analysed " + str(len(trips)) + " trips")
+    end_time = datetime(datetime.today().year,
+                        datetime.today().month,
+                        datetime.today().day,
+                        end_time.hour,
+                        end_time.minute,
+                        end_time.second)
+
+    trips = pd.read_pickle(_interim_data_dir + "/trips_with_delays.pickle")
+
+    trips_time = trips[((start_time < trips['start_timestamp']) & (trips['start_timestamp'] < end_time)) |
+                       ((start_time < trips['end_timestamp']) & (trips['end_timestamp'] < end_time))]
+
+    logging.info("Found trips between " + start_time + " and " + end_time)
+    trips_time.to_csv(_interim_data_dir + "/slice.csv")
 
 
-if __name__== "__main__":
-    data_dir = "data/" + time.strftime("%Y%m%d", time.localtime())
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    analyse_by_trip(data_dir)
+if __name__ == "__main__":
+    # run in own directory
+    os.chdir(os.path.dirname(sys.argv[0]))
+    raw_data_dir = "../../data/raw/" + datetime.strftime(datetime.now(), "%Y%m%d")
+    interim_data_dir = "../../data/interim/" + datetime.strftime(datetime.now(), "%Y%m%d")
+    log_dir = '../../data/logs/'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    logging.basicConfig(filename=log_dir+'train_analyse_trips.log', level=logging.INFO,
+                        format='%(asctime)s %(message)s')
+    from_time = time(7, 0, 0)
+    to_time = time(10, 0, 0)
+    analyse_by_trip(raw_data_dir, interim_data_dir, from_time, to_time)
