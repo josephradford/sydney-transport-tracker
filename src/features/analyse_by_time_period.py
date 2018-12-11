e98ed24f393d0f65b210a806fcd68d1116351fbf
Joseph Radford <joeradford@gmail.com>
2018-12-11 21:12:58 +1100

    Shorten time period for testing so it is faster

--- src/features/analyse_by_time_period.py
+++ src/features/analyse_by_time_period.py
@@ -13,6 +13,6 @@ def analyse_by_time_run(start_time, end_time, date_of_analysis):
 if __name__ == "__main__":
     # run in own directory
     os.chdir(os.path.dirname(sys.argv[0]))
-    from_time = time(7, 0, 0)
-    to_time = time(9, 0, 0)
+    from_time = time(7, 30, 0)
+    to_time = time(8, 0, 0)
     analyse_by_time_run(from_time, to_time, date.today())
