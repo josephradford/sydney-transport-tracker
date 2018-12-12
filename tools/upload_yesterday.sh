c36c5dcdd54d8c92e4afc11ee3fa50aa15ba1515
Joseph Radford <joeradford@gmail.com>
2018-12-12 22:01:03 +1100

    Use dropbox_uploader to zip, upload, and delete directories
    Does not delete original directory (yet)

---
+++ tools/upload_yesterday.sh
@@ -0,0 +1,9 @@
+# assumes dropbox_uploader.sh is installed and configured
+
+# zip the folder first
+YESTERDAY=$(date --date='yesterday' +"%Y%m%d")
+zip -r ${YESTERDAY}.zip ~/sydney-transport-tracker/data/raw/${YESTERDAY}
+
+# upload to dropbox
+./dropbox_uploader.sh upload ${YESTERDAY}.zip ${YESTERDAY}.zip 
+rm ${YESTERDAY}.zip 
