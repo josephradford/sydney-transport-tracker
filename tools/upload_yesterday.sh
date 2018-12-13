dde51ee0528d556e2ed4e298c963d4b0a88c66ac
Joseph Radford <joeradford@gmail.com>
2018-12-13 19:17:47 +1100

    Use full path so will find correct script when run as root

--- tools/upload_yesterday.sh
+++ tools/upload_yesterday.sh
@@ -2,7 +2,7 @@
 
 # zip the folder first
 YESTERDAY=$(date --date='yesterday' +"%Y%m%d")
-zip -r ${YESTERDAY}.zip ~/sydney-transport-tracker/data/raw/${YESTERDAY}
+zip -r ${YESTERDAY}.zip /home/pi/sydney-transport-tracker/data/raw/${YESTERDAY}
 
 # upload to dropbox
 ./dropbox_uploader.sh upload ${YESTERDAY}.zip ${YESTERDAY}.zip 
