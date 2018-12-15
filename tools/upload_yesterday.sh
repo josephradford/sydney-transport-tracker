6cabd9055a4f512021673ab1eec1d7724e984610
Joseph Radford <joeradford@gmail.com>
2018-12-15 18:20:18 +1100

    Add full path to dropbox_uploader so it can be run from any dir

--- tools/upload_yesterday.sh
+++ tools/upload_yesterday.sh
@@ -5,5 +5,5 @@ YESTERDAY=$(date --date='yesterday' +"%Y%m%d")
 zip -r ${YESTERDAY}.zip /home/pi/sydney-transport-tracker/data/raw/${YESTERDAY}
 
 # upload to dropbox
-./dropbox_uploader.sh upload ${YESTERDAY}.zip ${YESTERDAY}.zip 
+/home/pi/sydney-transport-tracker/tools/dropbox_uploader.sh upload ${YESTERDAY}.zip ${YESTERDAY}.zip
 rm ${YESTERDAY}.zip 
